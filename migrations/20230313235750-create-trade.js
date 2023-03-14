746e69dc5bc5084e1dbd42214a99ad73a1e1e0f2
kianu <thomasulee@gmail.com>
2023-03-13 20:03:42 -0400

    Postgres database configuration with sequelize migrations and addition to node server file

---
+++ migrations/20230313235750-create-trade.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Trades', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      offerorID: {
+        type: Sequelize.STRING
+      },
+      offereeID: {
+        type: Sequelize.STRING
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      itemID: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Trades');
+  }
+};
---
+++ models/trade.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Trade extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Trade.init({
+    offerorID: DataTypes.STRING,
+    offereeID: DataTypes.STRING,
+    status: DataTypes.STRING,
+    itemID: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Trade',
+  });
+  return Trade;
+};
