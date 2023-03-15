1ac690359716af14bdef79692eca5dd9868451be
kngsong1 <kristinagiannoutsos@gmail.com>
2023-03-14 23:58:24 -0500

    added endpoint for items, and deleting items. Need to work on logic to limit items to just one displaying

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
+
 app.use(
   session({
     secret: 'secret',
@@ -31,7 +32,40 @@ app.get('/heartbeat', (req, res) => {
     })
 });
 
-/*app.get('/check-auth', async(req, res) => {
+app.get('/list-items', async (req, res) => {
+  const items = await Item.findAll({
+      attributes: ['id','description', 'category', 'image', 'createdAt']
+  });
+  res.json(items);
+});
+
+app.post('/new-item', async (req, res) => {
+  // req.body contains an Object with firstName, lastName, email
+ const { description, image, category } = req.body;
+ const newPost = await Item.create({
+     description,
+     image,
+     category 
+ });
+ 
+ // Send back the new user's ID in the response:
+ res.json({
+     id: newPost.id
+})
+})
+
+app.delete('/delete-item/:id', async (req, res) => {
+  const { id } = req.params;
+  const removeItem = await Item.destroy({
+      where:
+       {
+          id
+      }
+  });
+  res.json(removeItem);
+});
+
+app.get('/check-auth', async(req, res) => {
   if(req.session.user) {
     res.send({
       isLoggedIn: !(req.session.user == null),
@@ -70,21 +104,23 @@ app.post('/login', async(req, res) => {
 app.post('/create_account', async(req, res) => {
   const user = await User.findAll({
     where: {
-      username: {
-        [Op.eq]: req.body.username
+      email: {
+        [Op.eq]: req.body.email
       }
     }
   });
   if(user[0] == null) {
     bcrypt.hash(req.body.passphrase, 10, function(err, hash) {
-        User.create({username: req.body.username, password: hash});
-        Folder.create({name: 'notes', user: req.body.username})
+        User.create({email: req.body.email, password: hash});
     });
     res.json({success: true, message: 'Create success'});
   } else {
-    res.json({success: false, message: 'Username or password invalid'});
+    res.json({success: false, message: 'Email or password invalid'});
   }
 });
+
+
+
 
 app.get('/logout', async(req, res) => {
   req.session.destroy();
@@ -96,6 +132,6 @@ app.get('/logout', async(req, res) => {
 /* Main app routes */
 
 
-const server = app.listen(3001, function() {
+const server = app.listen(3000, function() {
     console.log('listening on port 3001');
 });
