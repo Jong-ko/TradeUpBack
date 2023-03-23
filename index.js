bdca2e9bcf0d3abffe295a31783f6e2fe3e5ec22
kngsong1 <kristinagiannoutsos@gmail.com>
2023-03-23 17:58:07 -0500

    no changes from my end, should be good to go

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const session = require("express-session");
-const path = require("path");
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
 const app = express();
-const Sequelize = require("sequelize");
-const { User, Item, Trade } = require("./models");
-const { Op } = require("sequelize");
-const cookieParser = require("cookie-parser");
-const bcrypt = require("bcrypt");
+const Sequelize = require('sequelize');
+const { User, Item, Trade } = require('./models');
+const { Op } = require('sequelize');
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
 const multer = require('multer');
 const fs = require('fs');
 
@@ -19,7 +19,7 @@ app.use('/public/images', express.static('images'));
 
 app.use(
   session({
-    secret: "secret",
+    secret: 'secret',
     resave: false,
     saveUninitialized: true,
     cookie: {
@@ -30,71 +30,78 @@ app.use(
 );
 
 //test for accessibility
-app.get("/heartbeat", (req, res) => {
+app.get('/heartbeat', (req, res) => {
   res.json({
-    is: "working",
+    is: 'working',
   });
 });
 
-app.get("/list-items", async (req, res) => {
+app.get('/list-items', async (req, res) => {
   const items = await Item.findAll({
-      where: {
-        userAccount: req.session.user
-      }
+    where: {
+      userAccount: req.session.user,
+    },
   });
   res.json(items);
 });
 
 app.post('/new-item', upload.single('image'), async (req, res) => {
   // req.body contains an Object with firstName, lastName, email
- const { description, category, userAccount } = req.body;
- const image = req.file.filename;
- 
- const newPost = await Item.create({
-     description,
-     image,
-     category,
-     userAccount, 
- });
- 
- // Send back the new user's ID in the response:
- res.send([{
-  "description": description,
-  "image": image,
-  "category": category
-}])
-})
+  const { description, category, userAccount } = req.body;
+  const image = req.file.filename;
+
+  const newPost = await Item.create({
+    description,
+    image,
+    category,
+    userAccount,
+  });
+
+  // Send back the new user's ID in the response:
+  res.send([
+    {
+      description: description,
+      image: image,
+      category: category,
+    },
+  ]);
+});
 
 app.post('/update-item', upload.single('image'), async (req, res) => {
   // req.body contains an Object with firstName, lastName, email
- const { description, category, userAccount } = req.body;
- const image = req.file.filename;
- 
- const newPost = await Item.update({
-     description,
-     image,
-     category, 
- },{
-  where:{
-    userAccount: userAccount
-  }
- });
- 
- // Send back the new user's ID in the response:
- res.send([{
-     "description": description,
-     "image": image,
-     "category": category
-}])
-})
+  const { description, category, userAccount } = req.body;
+  const image = req.file.filename;
+
+  const newPost = await Item.update(
+    {
+      description,
+      image,
+      category,
+    },
+    {
+      where: {
+        userAccount: userAccount,
+      },
+    }
+  );
+
+  // Send back the new user's ID in the response:
+  res.send([
+    {
+      description: description,
+      image: image,
+      category: category,
+    },
+  ]);
+});
 
 app.get('/images/:imageName', (req, res) => {
   const imageName = req.params.imageName;
   const readStream = fs.createReadStream(`public/images/${imageName}`);
   readStream.pipe(res);
-})
+});
 
-app.delete("/delete-item/:id", async (req, res) => {
+app.delete('/delete-item/:id', async (req, res) => {
   const { id } = req.params;
   const removeItem = await Item.destroy({
     where: {
@@ -104,7 +111,9 @@ app.delete("/delete-item/:id", async (req, res) => {
   res.json(removeItem);
 });
 
-app.get("/check-auth", async (req, res) => {
+//Routes for User Login, Logout, Create Account and authentication
+
+app.get('/check-auth', async (req, res) => {
   if (req.session.user) {
     res.send({
       isLoggedIn: !(req.session.user == null),
@@ -113,12 +122,12 @@ app.get("/check-auth", async (req, res) => {
   } else {
     res.send({
       isLoggedIn: !(req.session.user == null),
-      email: "unassigned",
+      email: 'unassigned',
     });
   }
 });
 
-app.post("/login", async (req, res) => {
+app.post('/login', async (req, res) => {
   const user = await User.findAll({
     where: {
       email: {
@@ -127,20 +136,24 @@ app.post("/login", async (req, res) => {
     },
   });
   if (user[0] == null) {
-    res.json({ success: false, message: "Email or password invalid" });
+    res.json({ success: false, message: 'Email or password invalid' });
   } else {
     bcrypt.compare(req.body.password, user[0].password, function (err, result) {
       if (result && req.body.email === user[0].email) {
         req.session.user = req.body.email;
-        res.json({ success: true, message: "Login success", userID: user[0].id });
+        res.json({
+          success: true,
+          message: 'Login success',
+          userID: user[0].id,
+        });
       } else {
-        res.json({ success: false, message: "Email or password invalid" });
+        res.json({ success: false, message: 'Email or password invalid' });
       }
     });
   }
 });
 
-app.post("/create_account", async (req, res) => {
+app.post('/create_account', async (req, res) => {
   const user = await User.findAll({
     where: {
       email: {
@@ -150,15 +163,20 @@ app.post("/create_account", async (req, res) => {
   });
   if (user[0] == null) {
     bcrypt.hash(req.body.password, 10, function (err, hash) {
-      User.create({ email: req.body.email, password: hash });
+      User.create({
+        email: req.body.email,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        password: hash,
+      });
     });
-    res.json({ success: true, message: "Create success" });
+    res.json({ success: true, message: 'Create success' });
   } else {
-    res.json({ success: false, message: "Email or password invalid" });
+    res.json({ success: false, message: 'Email or password invalid' });
   }
 });
 
-app.get("/logout", async (req, res) => {
+app.get('/logout', async (req, res) => {
   req.session.destroy();
   res.send({
     isLoggedIn: false,
@@ -167,13 +185,13 @@ app.get("/logout", async (req, res) => {
 
 //routes for Item Browsing
 //All Item Fetch
-app.get("/fetchAllItems", async (req, res) => {
+app.get('/fetchAllItems', async (req, res) => {
   const items = await Item.findAll();
   res.json(items);
 });
 
 //Catagory Fetch used in ItemBrowsePage.js component
-app.get("/fetchCatagory/:catagory", async (req, res) => {
+app.get('/fetchCatagory/:catagory', async (req, res) => {
   const { catagory } = req.params;
   const items = await Item.findAll({
     where: {
@@ -184,31 +202,31 @@ app.get("/fetchCatagory/:catagory", async (req, res) => {
 });
 
 //Route for My Item
-app.get("/myItem/:userId", async (req, res) => {
+app.get('/myItem/:userId', async (req, res) => {
   const { userId } = req.params;
   const myItem = await Item.findAll({
     where: {
       userID: userId,
     },
   });
-  res.json(myItem)
+  res.json(myItem);
 });
 
 //Route to Trade Item used in TradeButton.js component
-app.post("/Trade", async(req,res) => {
-  const { offerorID, offereeID, itemID} =req.body;
+app.post('/Trade', async (req, res) => {
+  const { offerorID, offereeID, itemID } = req.body;
   const NewTrade = await Trade.create({
     offerorID,
     offereeID,
-    itemID
-  })
+    itemID,
+  });
   res.json({
-    id:NewTrade.ID,
-  })
-})
+    id: NewTrade.ID,
+  });
+});
 
 /* Main app routes */
 
 const server = app.listen(3001, function () {
-  console.log("listening on port 3001");
+  console.log('listening on port 3001');
 });
